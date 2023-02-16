6a761141b7464b65be1afd231c95e39b27790295
myUsername <jobzewde@gmail.com>
2023-02-16 11:45:00 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-     puts("\"programming is like building a multilingualtcome puzzle"\n");
-    
-    return(0);
-}
+	puts("\ "Programming is like building a multilingual puzzle);
+    return (0);
+} 
+
