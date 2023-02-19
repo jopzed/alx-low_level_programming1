a80a70b71830f0b6f80f823f57d2b173c46bf91a
myUsername <jobzewde@gmail.com>
2023-02-19 09:41:23 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
-* main - prints "with proper grammar, but the outcome is 
-* a piece of art," followed newline
-* Return: Always 0
+* main - Entry point
+* Return: Always 0 (succe)
 */
 
 int main(void)
